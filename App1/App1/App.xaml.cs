f22073b290e7da19bb87622c4266d99bb53e03c0
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-11-25 17:52:11 +0100

    Relative Layout

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new AbsolutePage();
+            MainPage = new RelativePage();
         }
 
         protected override void OnStart()
