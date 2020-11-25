13fdddc89e9d48d4c0041a5e52cfd88e16db0928
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-11-25 14:35:28 +0100

    GridLayout

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new StackPage();
+            MainPage = new GridPage();
         }
 
         protected override void OnStart()
