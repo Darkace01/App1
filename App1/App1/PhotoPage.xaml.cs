9fd23a8b71d0d78429465cb98cfe7cea4057a172
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-11-30 13:13:37 +0100

    PLatform Specific Images

--- App1/App1/PhotoPage.xaml.cs
+++ App1/App1/PhotoPage.xaml.cs
@@ -16,7 +16,7 @@ namespace App1
         {
             InitializeComponent();
 
-            //Image.Source = ImageSource.FromResource("App1.Images.background.jpg");
+           //btn.Image =(FileImageSource) ImageSource.FromFile("edge.png");
         }
     }
 }
