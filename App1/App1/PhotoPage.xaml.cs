8a02a0cdb6cab06d79a9f3ba90e53e441a594ea7
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-11-30 08:58:12 +0100

    Adding Image using URI

---
+++ App1/App1/PhotoPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhotoPage : ContentPage
+    {
+        public PhotoPage()
+        {
+            InitializeComponent();
+
+            //First method
+            //var imageSource = (UriImageSource) ImageSource.FromUri(new Uri("http://.."));
+
+            var imageSource = new UriImageSource() { Uri = new Uri("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg") };
+            imageSource.CachingEnabled = false;
+
+            Image.Source = imageSource;
+        }
+    }
+}
