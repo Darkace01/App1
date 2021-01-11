2fdc3695910de2c9698d871795a1dd36638d97dc
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2021-01-11 10:12:27 +0100

    Populating Cell

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new PhotoPage();
+            MainPage = new Demo();
         }
 
         protected override void OnStart()
---
+++ App1/App1/Demo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Demo : ContentPage
+    {
+        public Demo()
+        {
+            InitializeComponent();
+
+            
+            listView.ItemsSource = new List<Contact>
+            {
+                new Contact {Name = "Kazeem", ImageUrl = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"},
+                new Contact {Name = "John", ImageUrl = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg", Status = "Hey lets talk"}
+            };
+        }
+    }
+}
