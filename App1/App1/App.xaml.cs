165c74367c070dc99632b819ba403f24afcfb68f
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2021-01-15 10:12:36 +0100

    Tabbed Page

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new ContactMasterPage();
+            MainPage = new TabPage();
         }
 
         protected override void OnStart()
